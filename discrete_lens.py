3daa9fda9255165cbf36472061fa6cec74e14c16
andreistaicu1 <55329808+andreistaicu1@users.noreply.github.com>
2021-08-04 15:38:47 -0400

    stuff

---
+++ discrete_lens.py
@@ -0,0 +1,211 @@
+from light import *
+import xlwt
+
+lens_count = 20
+angle_accepted = m.pi / 8
+sample = 400
+radius_circle = 350
+lens_height = 1
+focal_length_discrete = 250
+size = Vector(800, 800)
+
+
+def can_consider(angle):
+    return (angle_accepted > angle >= 0) or ((-1 * angle_accepted) < angle <= 0)
+
+
+class Lens:
+
+    def __init__(self, area, focal_length, edge_1, edge_2):
+        self.area = area
+        self.focal_length = focal_length
+        self.edge_1 = edge_1
+        self.edge_2 = edge_2
+
+        edge_2.scale(-1)
+        lens_axis_v = edge_1.add(edge_2)
+        self.lens_axis = Vector(lens_axis_v.x, lens_axis_v.y)
+        edge_2.scale(-1)
+
+        self.lens_face_dir = lens_axis_v
+        self.lens_face_dir.rotate(-1 * m.pi / 2)
+        self.lens_face_dir.scale(1 / lens_axis_v.magnitude())
+
+        center_lens = edge_2.add(Vector((1 / 2) * self.lens_axis.x, (1 / 2) * self.lens_axis.y))
+        self.center_lens = center_lens
+        self.focal_point = center_lens.add(Vector(self.lens_face_dir.x * self.focal_length,
+                                                  self.lens_face_dir.y * self.focal_length))
+
+    def incident_area(self, angle):
+        return m.cos(angle) * self.area
+
+    def calculate_focal_point_adjust(self, angle):
+        change = self.focal_length * (m.tan(angle))
+        focal_point_v = Vector(self.lens_axis.x, self.lens_axis.y)
+        focal_point_v.scale(change / focal_point_v.magnitude())
+        return focal_point_v
+
+    def mark_area(self, lens_num, angle, sheet, angle_index):
+        if not can_consider(angle):
+            sheet.write(angle_index + 1, lens_num + 1, '0')
+            return
+
+        area = self.incident_area(angle)
+        sheet.write(angle_index + 1, lens_num + 1, str(area))
+
+
+def fill_excel_sheet(lenses_list):
+    work = xlwt.Workbook()
+    sheet1 = work.add_sheet('First sample')
+
+    interval = (2 * m.pi) / sample
+    for i in range(sample):
+        angle = i * interval
+        sheet1.write(i + 1, 0, str(angle))
+        for index_lens in range(len(lenses_list)):
+
+            unit = Vector(1, 0)
+            unit.rotate(angle)
+            dot = (lenses_list[index_lens].lens_face_dir.x * unit.x) + (lenses_list[index_lens].lens_face_dir.y * unit.y)
+            cross = (lenses_list[index_lens].lens_face_dir.x * unit.y) - (lenses_list[index_lens].lens_face_dir.y * unit.x)
+            if dot > 1:
+                dot = 1
+            elif dot < -1:
+                dot = -1
+            angle_to_lens = m.acos(dot)
+
+            if cross < 0:
+                angle_to_lens *= -1
+
+            lenses_list[index_lens].mark_area(index_lens, angle_to_lens, sheet1, i)
+
+    work.save('first_sample_data.xls')
+
+
+def create_lenses(num_lenses):
+    if num_lenses < 3:
+        return
+
+    lenses_list = []
+
+    interval = (2 * m.pi) / num_lenses
+    for i in range(num_lenses):
+        first_edge_rot = i * interval
+        second_edge_rot = ((i + 1) % num_lenses) * interval
+
+        radius = Vector(1, 0)
+        radius.scale(radius_circle)
+
+        edge_1 = Vector(radius.x, radius.y)
+        edge_1.rotate(first_edge_rot)
+        edge_1 = edge_1.add(Vector((1 / 2) * size.x, (1 / 2) * size.y))
+
+        edge_2 = Vector(radius.x, radius.y)
+        edge_2.rotate(second_edge_rot)
+        edge_2 = edge_2.add(Vector((1 / 2) * size.x, (1 / 2) * size.y))
+
+        axis = edge_1.add(Vector(-1 * edge_2.x, -1 * edge_2.y))
+        width = axis.magnitude()
+
+        single_lens = Lens(lens_height * width, focal_length_discrete, edge_1, edge_2)
+        lenses_list.append(single_lens)
+
+    return lenses_list
+
+
+def draw_lenses(list_lenses, canvas_s):
+    for lens_index in range(len(list_lenses)):
+        item = list_lenses[lens_index]
+        if lens_index == 0:
+            canvas_s.create_line(item.edge_1.x, item.edge_1.y, item.edge_2.x, item.edge_2.y, fill='blue')
+            draw_circle(canvas_s, 'blue', 8, (item.focal_point.x, item.focal_point.y))
+            draw_circle(canvas_s, 'blue', 2, (item.center_lens.x, item.center_lens.y))
+            draw_circle(canvas_s, 'red', 3, (item.edge_1.x, item.edge_1.y))
+            draw_circle(canvas_s, 'black', 3, (item.edge_2.x, item.edge_2.y))
+        else:
+            canvas_s.create_line(item.edge_1.x, item.edge_1.y, item.edge_2.x, item.edge_2.y, fill='black')
+            draw_circle(canvas_s, 'black', 2, (item.focal_point.x, item.focal_point.y))
+
+
+def get_dict_focal_points(list_lenses):
+    focal_dict = {}
+    interval = (2 * m.pi) / sample
+
+    for i in range(sample):
+        angle = i * interval
+        values = []
+        for single_lens in list_lenses:
+
+            unit = Vector(1, 0)
+            unit.rotate(angle)
+            dot = (single_lens.lens_face_dir.x * unit.x) + (single_lens.lens_face_dir.y * unit.y)
+            cross = (single_lens.lens_face_dir.x * unit.y) - (single_lens.lens_face_dir.y * unit.x)
+            if dot > 1:
+                dot = 1
+            elif dot < -1:
+                dot = -1
+            angle_to_lens = m.acos(dot)
+
+            if cross < 0:
+                angle_to_lens *= -1
+
+            if can_consider(angle_to_lens):
+                focal_point_move = single_lens.calculate_focal_point_adjust(angle_to_lens)
+                true_focal = single_lens.focal_point.add(focal_point_move)
+                values.append(true_focal)
+            else:
+                values.append(Vector(0, 0))
+
+        focal_dict[i] = values
+
+    return focal_dict
+
+
+def key_handler(event):
+    """Handle key presses."""
+    global handle_list
+    global current_angle
+
+    if event.keysym == 'q':
+        quit()
+
+    if event.keysym == 'a':
+        for thing in handle_list:
+            c.delete(thing)
+        handle_list = []
+        new_angle = (current_angle + 1) % sample
+        points = focal_point_dict[new_angle]
+        for item in points:
+            handle_list.append(draw_circle(c, 'red', 2, (item.x, item.y)))
+        current_angle = new_angle
+
+    if event.keysym == 'd':
+        for thing in handle_list:
+            c.delete(thing)
+        handle_list = []
+        new_angle = current_angle - 1
+        if new_angle < 0:
+            new_angle = sample - 1
+        points = focal_point_dict[new_angle]
+        for item in points:
+            handle_list.append(draw_circle(c, 'red', 2, (item.x, item.y)))
+        current_angle = new_angle
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.geometry('800x800')
+    c = Canvas(root, width=800, height=800)
+    c.pack()
+
+    fill_excel_sheet(create_lenses(lens_count))
+
+    lenses = create_lenses(lens_count)
+    draw_lenses(lenses, c)
+    focal_point_dict = get_dict_focal_points(lenses)
+    handle_list = []
+    root.bind('<q>', key_handler)
+    root.bind('<a>', key_handler)
+    root.bind('<d>', key_handler)
+    current_angle = 0
+    root.mainloop()
---
+++ light.py
@@ -0,0 +1,145 @@
+from tkinter import *
+import math as m
+
+focal_length_r = 150
+focal_length_small_factor = 10
+radial_length_r = 300
+sample_size_r = 500
+sample_length_r = m.pi / 4
+incoming_light_angle = m.pi
+factor_sample = 4
+
+
+class Vector:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def scale(self, scalar):
+        self.x *= scalar
+        self.y *= scalar
+
+    def rotate(self, angle):
+        # possible problem?
+        new_x = (self.x * m.cos(angle)) - (self.y * m.sin(angle))
+        new_y = (self.x * m.sin(angle)) + (self.y * m.cos(angle))
+        self.x = new_x
+        self.y = new_y
+
+    def magnitude(self):
+        return m.sqrt(m.pow(self.x, 2) + m.pow(self.y, 2))
+
+    def add(self, v):
+        return Vector(self.x + v.x, self.y + v.y)
+
+
+class TorusLens:
+
+    def __init__(self, focal_length, radial_length):
+        self.focal_length = focal_length
+        self.radial_length = radial_length
+
+    def get_focal_point(self, angle, radius_vector, factor):
+        adjust_distance = abs(m.tan(angle)) * self.focal_length
+        # adjust_distance = 0
+
+        normal_vector = Vector(radius_vector.x, radius_vector.y)
+        normal_vector.rotate(m.pi / 2)
+        normal_vector.scale(((factor * adjust_distance) / normal_vector.magnitude()))
+
+        focal_point = Vector(radius_vector.x, radius_vector.y)
+        focal_point.scale(1 - (self.focal_length / self.radial_length))
+
+        return focal_point.add(normal_vector)
+
+
+def draw_circle(canvas_s, color, radius, center):
+    (x, y) = center
+    start_x = x - radius
+    start_y = y - radius
+    finish_x = x + radius
+    finish_y = y + radius
+    return canvas_s.create_oval(start_x, start_y, finish_x, finish_y, outline=color)
+
+
+def generate_plot(canvas_s, sample_size, sample_length, torus_lens):
+    center = Vector(400, 400)
+    split_sphere = sample_size * factor_sample
+    incoming_light = Vector(1, 0)
+    incoming_light.rotate(incoming_light_angle)
+
+    box = []
+
+    for i in range(split_sphere):
+
+        angle = (m.pi * 2) * (i / split_sphere)
+        radial_point = Vector(torus_lens.radial_length, 0)
+        radial_point.rotate(angle)
+        dot = (radial_point.x * incoming_light.x) + (radial_point.y * incoming_light.y)
+
+        if dot > 0:
+            angle_to_lens = m.acos(dot / radial_point.magnitude())
+            if angle_to_lens < (m.pi / factor_sample):
+                factor = 1
+
+                cross = (radial_point.x * incoming_light.y) - (radial_point.y * incoming_light.x)
+                if cross > 0:
+                    factor = -1
+
+                focal_point = torus_lens.get_focal_point(angle_to_lens, radial_point, factor)
+
+                box.append(focal_point)
+
+                graph_focal = focal_point.add(center)
+                graph_radial = radial_point.add(center)
+                draw_circle(canvas_s, 'black', 2, (graph_focal.x, graph_focal.y))
+                draw_circle(canvas_s, 'blue', 2, (graph_radial.x, graph_radial.y))
+                # canvas_s.create_line(graph_radial.x, graph_radial.y, graph_focal.x, graph_focal.y, fill='red')
+
+        max_magnitude = 0
+        square_radius = Vector(0, 0)
+
+        small_focal_vector = Vector(-1 * focal_length_r * (1 / focal_length_small_factor), 0)
+        small_focal_vector.rotate(incoming_light_angle)
+
+        new_box = []
+
+        for coordinate in box:
+
+            small_lens_loc = coordinate.add(small_focal_vector)
+            new_box.append(small_lens_loc)
+            graph_small = small_lens_loc.add(center)
+            draw_circle(canvas_s, 'red', 2, (graph_small.x, graph_small.y))
+
+        for coordinate in new_box:
+
+            if coordinate.magnitude() > max_magnitude:
+                if coordinate.x > coordinate.y:
+                    square_radius = Vector(coordinate.x, coordinate.x)
+                else:
+                    square_radius = Vector(coordinate.y, coordinate.y)
+                max_magnitude = coordinate.magnitude()
+
+        end_point = Vector(-1 * square_radius.x, -1 * square_radius.y)
+
+        square = [square_radius, end_point]
+        draw_square = []
+
+        for item in square:
+            draw_square.append(item.add(center))
+
+        canvas_s.create_oval(draw_square[0].x, draw_square[0].y, draw_square[1].x, draw_square[1].y, outline='red')
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.geometry('800x800')
+    c = Canvas(root, width=800, height=800)
+    c.pack()
+
+    lens = TorusLens(focal_length_r, radial_length_r)
+    draw_circle(c, 'black', radial_length_r, (400, 400))
+    generate_plot(c, sample_size_r, sample_length_r, lens)
+
+    root.mainloop()
